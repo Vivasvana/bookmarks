d1278c935aaee138810dfc9eab82944ef74c34d6
yshevchenko <yevhen.shevchenko@evia.de>
2020-08-06 22:22:08 +0300

    Add an angular material

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import {MatButtonModule} from "@angular/material/button";
+
+const Material = [MatButtonModule];
+
+@NgModule({
+  declarations: [],
+  imports: [Material],
+  exports: [Material]
+})
+export class MaterialModule { }
